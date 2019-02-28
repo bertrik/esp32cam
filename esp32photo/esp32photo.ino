0439b31d8cb669f6afb21225cdb460dffd9a87ca
Bertrik Sikken <bertrik@sikken.nl>
2019-02-28 22:49:36 +0100

    Implement basic command interpreter

--- esp32photo/esp32photo.ino
+++ esp32photo/esp32photo.ino
@@ -3,14 +3,64 @@
 #include <Arduino.h>
 
 #include "print.h"
+#include "editline.h"
+#include "cmdproc.h"
+
+static char line[80];
 
 void setup(void)
 {
     PrintInit(115200);
     print("\nESP32-CAM!\n");
+    
+    EditInit(line, sizeof(line));
+}
+
+static void show_help(const cmd_t *cmds)
+{
+    for (const cmd_t *cmd = cmds; cmd->cmd != NULL; cmd++) {
+        print("%10s: %s\n", cmd->name, cmd->help);
+    }
+}
+
+static int do_help(int argc, char *argv[]);
+
+const cmd_t commands[] = {
+    {"help",    do_help,    "Show help"},
+    {NULL, NULL, NULL}
+};
+
+static int do_help(int argc, char *argv[])
+{
+    show_help(commands);
+    return 0;
 }
 
 void loop(void)
 {
+    bool haveLine = false;
+    if (Serial.available()) {
+        char c;
+        haveLine = EditLine(Serial.read(), &c);
+        Serial.print(c);
+    }
+    if (haveLine) {
+        int result = cmd_process(commands, line);
+        switch (result) {
+        case CMD_OK:
+            print("OK\n");
+            break;
+        case CMD_NO_CMD:
+            break;
+        case CMD_UNKNOWN:
+            print("Unknown command, available commands:\n");
+            show_help(commands);
+            break;
+        default:
+            print("%d\n", result);
+            break;
+        }
+        print(">");
+    }
 }
 
